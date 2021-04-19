196d4e876fee217fbe371ec3832dce22d11a8f88
Timi <ti.mi.34@hotmail.fr>
2021-04-19 17:50:37 +0200

    add updateType

--- server/routes/adminRoutes/account/index.js
+++ server/routes/adminRoutes/account/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.patch('/updateMail', require("./updateMail"));
+router.patch('/updateType', require("./updateType"));
 router.post('/getByMail', require("./getAccountByMail"))
 router.get('/getAll', require("./getAllAccounts"))
 
--- server/routes/adminRoutes/account/updateType.js
+++ server/routes/adminRoutes/account/updateType.js
@@ -2,12 +2,12 @@ const AccountController = require('../../../controllers/accountController');
 
 module.exports = async (req, res) => {
     try{
-        const account = await AccountController.getAccountByEmail(req.body.accountMail);
+        const account = await AccountController.updateType(req.body.accountId, req.body.accountType);
         return res.status(200).json(account);
     }
     catch(e){
         return res.status(500).json({
-            error : "Impossible de récupérer cet account"
+            error : "Impossible de modifier cet account"
         });
     }
 };
