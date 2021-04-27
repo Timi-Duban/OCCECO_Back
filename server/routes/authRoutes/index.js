0c9919c3f6477ac954ab8fdf9ab53bd62f5d5e43
Timi <ti.mi.34@hotmail.fr>
2021-04-27 12:41:09 +0200

    reset password part1

--- server/routes/authRoutes/index.js
+++ server/routes/authRoutes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 router.post('/login', require("./login"));
 router.post('/signupPublic', require("./signupPublic"));
+router.post('/forgotPassword', require("./forgotPassword"));
 
 
 module.exports =  router;
